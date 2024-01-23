27d8b785b4fffa05c0662ce3948f3be478b5e095
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-23 14:41:03 -0300

    data types in ts

--- data_types/main.js
+++ data_types/main.js
@@ -3,17 +3,36 @@ let numbers = [1, 2, 3, 4];
 numbers.push(5);
 console.log(numbers[2]);
 const names = ['Ana', 'Kamylle'];
-console.log(names[0]);
-const num = [100, 200]; //sintaxe alternativa
-console.log(num[0]);
-//parametros tipados
-function sum(a, b) {
-    console.log(a + b);
-}
-sum(4, 4);
-//funções anonimas
-setTimeout(function () {
-    const sallary = 2000;
-    //console.log(parseFloat(sallary))
-    //console.log(sallary)
-}, 2000);
+//data types
+let persons = 3; //number
+const validNum = true; //boolean
+const lastName = 'Kamylle'; //string
+let idk = 5; //the any variable adapts according to the data that is assigned to it, therefore, it is a bit contradictory to the typescript check
+idk = true;
+//lists
+const numberList = [1, 2, 3, 4, 5]; //number list
+const booleanList = [true, false, false, true]; //boolean list
+const stringList = ['Lucas', 'Kamylle', 'Lili']; //string list
+//tuples
+const peopleList = [21, 'Lucas', true];
+//lits of tuples
+const totalPeople = [
+    [20, 'Kamylle'],
+    [21, 'Lucas'],
+];
+//We inform the types that must be entered in order by the user, that is, it is a list with a number and a string, the order of information cannot change
+//intersections
+const productIDs = '1';
+//when we have intersections, we have that a value can vary, in this case it is a string and a number
+//enum
+var turnOn;
+(function (turnOn) {
+    turnOn[turnOn["on"] = 1] = "on";
+    turnOn[turnOn["off"] = 2] = "off";
+})(turnOn || (turnOn = {}));
+const turn = turnOn.on; //1
+//We use the enum to assign a value x to a property y:
+//type Assertions:
+const itemName = 'Cap';
+const itemType = itemName;
+//its a variable treatment, when we need to attribute a type for a variable
--- data_types/main.ts
+++ data_types/main.ts
@@ -1,29 +1,55 @@
+
 let numbers: number[] = [1,2,3,4]
 numbers.push(5)
 console.log(numbers[2])
 
-
 const names: string[] = ['Ana', 'Kamylle']
-console.log(names[0])
+
+//data types
+let persons: number = 3 //number
+const validNum: boolean = true //boolean
+const lastName: string = 'Kamylle' //string
+let idk: any = 5 //the any variable adapts according to the data that is assigned to it, therefore, it is a bit contradictory to the typescript check
+idk = true
 
 
-const num: Array<number> = [100,200] //sintaxe alternativa
-console.log(num[0])
 
-//parametros tipados
-function sum(a:number, b:number){
-    console.log(a+b)
+//lists
+const numberList: number[] = [1,2,3,4,5] //number list
+const booleanList: boolean[] = [true,false,false,true] //boolean list
+const stringList: string[] = ['Lucas', 'Kamylle', 'Lili'] //string list
+
+
+
+//tuples
+const peopleList: [number,string,boolean] = [21, 'Lucas', true]
+
+
+//lits of tuples
+const totalPeople: [number,string][] = [
+    [20, 'Kamylle'],
+    [21, 'Lucas'],
+]
+//We inform the types that must be entered in order by the user, that is, it is a list with a number and a string, the order of information cannot change
+
+
+
+//intersections
+const productIDs: string | number = '1'
+//when we have intersections, we have that a value can vary, in this case it is a string and a number
+
+
+
+//enum
+enum turnOn{
+    on = 1,
+    off = 2
 }
+const turn = turnOn.on //1
+//We use the enum to assign a value x to a property y:
 
-sum(4,4)
 
-//funções anonimas
-setTimeout(function(){
-    const sallary: number = 2000;
-    
-    //console.log(parseFloat(sallary))
-
-    //console.log(sallary)
-
-}, 2000)
-
+//type Assertions:
+const itemName: any = 'Cap'
+const itemType = itemName as string
+//its a variable treatment, when we need to attribute a type for a variable
