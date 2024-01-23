81633a90f16e5df688909908787f3eabdd622f32
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-01-22 22:20:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+const firstName = 'lucas';
+const anotherName = 'kamylle';
+function greetings(name) {
+    console.log('Hello!' + name + ',welcome');
+}
+greetings(anotherName);
+console.log('testing...');
---
+++ index.ts
@@ -0,0 +1,12 @@
+const firstName: string = 'lucas';
+const anotherName:string = 'kamylle'
+
+
+function greetings(name: string){
+    console.log('Hello!' + name + ',welcome');
+}
+
+greetings(anotherName)
+
+
+console.log('testing...')
