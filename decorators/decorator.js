374a1b01108bd3baf0c46f02a85276044051fe6b
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-02-23 09:21:07 -0300

    decorator with accessor

--- decorators/decorator.js
+++ decorators/decorator.js
@@ -5,8 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-
-//decorator de classe
+//decorator and classes
 class User {
     constructor(name) {
         this.name = name;
@@ -14,8 +13,7 @@ class User {
 }
 const kamylle = new User("Kamylle");
 console.log(kamylle);
-
-//decorator e metodos
+//decorator and methods 
 function enumerable(value) {
     return function (target, propertyKey, descriptor) {
         descriptor.enumerable = value;
@@ -35,3 +33,52 @@ __decorate([
 const trator = new Machine("Trator");
 trator.showName();
 console.log(trator);
+//decorator and classes
+function log(target, propertyKey, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (args) {
+        console.log(`Method ${name} called with json args: %{JSON.string}`);
+    };
+}
+//decorator and accessor
+class Monster {
+    constructor(name, age) {
+        this.name = name,
+            this.age = age;
+    }
+    get showName() {
+        return `The monster's name is ${this.name}`;
+    }
+    get showAge() {
+        return `The monster's age is: ${this.age}`;
+    }
+}
+__decorate([
+    enumerable(true)
+], Monster.prototype, "showName", null);
+const vampire = new Monster("Vampire", 100);
+console.log(vampire);
+//decorator and accessor part II
+function mySecondDecorator() {
+    return function (target, propertyKey, descriptor) {
+        const originalMethod = descriptor.get;
+        descriptor.get = function () {
+            console.log(`Calling the property ${propertyKey}`);
+            return originalMethod === null || originalMethod === void 0 ? void 0 : originalMethod.call(this);
+        };
+        return descriptor;
+    };
+}
+class mySecondClass {
+    constructor() {
+        this._myProperty = 'This is my private property!';
+    }
+    get myProperty() {
+        return this._myProperty;
+    }
+}
+__decorate([
+    mySecondDecorator()
+], mySecondClass.prototype, "myProperty", null);
+const instance = new mySecondClass();
+console.log(instance.myProperty);
--- decorators/decorator.ts
+++ decorators/decorator.ts
@@ -1,4 +1,4 @@
-//decorator de classe
+//decorator and classes
 class User{
     name
     constructor(name: string){
@@ -8,8 +8,9 @@ class User{
 const kamylle = new User("Kamylle")
 console.log(kamylle)
 
-//decorator e metodos
 
+
+//decorator and methods 
 function enumerable(value: boolean){
     return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
         descriptor.enumerable = value;
@@ -29,3 +30,66 @@ class Machine{
 const trator = new Machine("Trator")
 trator.showName()
 console.log(trator)
+
+
+//decorator and classes
+function log(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+){
+    const originalMethod = descriptor.value
+    descriptor.value = function(args: any[]){
+        console.log(`Method ${name} called with json args: %{JSON.string}`)
+    }
+}
+
+//decorator and accessor
+class Monster{
+    name?
+    age?
+    constructor(name: string, age:number){
+        this.name = name,
+        this.age = age
+    }
+
+    @enumerable(true)
+    get showName(){
+        return `The monster's name is ${this.name}`
+    }
+
+    get showAge(){
+        return `The monster's age is: ${this.age}`
+    }
+}
+
+const vampire = new Monster("Vampire", 100)
+console.log(vampire)
+
+
+//decorator and accessor part II
+function mySecondDecorator(){
+    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+        const originalMethod = descriptor.get;
+
+        descriptor.get = function(){
+            console.log(`Calling the property ${propertyKey}`)
+            return originalMethod?.call(this)
+        }
+
+        return descriptor
+    }
+}
+
+class mySecondClass{
+    private _myProperty: string = 'This is my private property!'
+
+    @mySecondDecorator()
+    get myProperty():string{
+        return this. _myProperty;
+    }
+
+}
+
+const instance = new mySecondClass()
+console.log(instance.myProperty)
